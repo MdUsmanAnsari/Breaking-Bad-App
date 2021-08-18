58a7da368c896cd26b5cdde25267edf19b2906c8
Md.Usman Ansari <40880948+MdUsmanAnsari@users.noreply.github.com>
2021-08-18 21:50:07 +0530

    Error fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Character from "./Character";
+import Character from "./character";
 import Characters from "./Characters";
 import ErrorPage from "./ErrorPage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [ isdataLoading, setDataLoading] = useState(true);
-  const [ characters, setCharacters ] = useState([]);
+  const [isdataLoading, setDataLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetch("https://breakingbadapi.com/api/characters")
-    .then(res => res.json())
-    .then(data => {
-      setCharacters(data);
-      setDataLoading(false);
-    });
+      .then(res => res.json())
+      .then(data => {
+        setCharacters(data);
+        setDataLoading(false);
+      });
   }, []);
 
   return (
